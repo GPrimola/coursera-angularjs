97e65b3cb1300640807d00c992221c47e0540ce5
Giorgio Torres <torres.giorgio@gmail.com>
2016-10-16 01:13:08 -0200

    Assignment bonus.

--- module1-assignment-solution/scripts/app.js
+++ module1-assignment-solution/scripts/app.js
@@ -7,14 +7,16 @@
 	LunchCheckController.$inject = ['$scope', '$filter'];
 	function LunchCheckController($scope) {
 		$scope.checkLunchList = function() {
+			$scope.color = 'red';
 			var msg = 'Please enter data first';
 			if($scope.lunchList) {
+				$scope.color = 'green'
+				$scope.alert = 'We\'re not counting empty item! (e.g.: "L1, ,L3," counts 2 elements)'
 				if($scope.lunchList.split(',').filter(elementsNotEmpty).length <= 3) {
 					msg = 'Enjoy!';
 				} else {
 					msg = 'Too much!';
 				}
-				$scope.alert = 'We\'re not counting empty item! (e.g.: "L1, ,L3," counts 2 elements)'
 			}
 			$scope.resultMessage = msg;
 		}
